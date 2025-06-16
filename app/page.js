f04be7986cdd85fb0031417ae93274eb90574cdb
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-06-16 13:13:13 +0200

    added basic socket implementation where you can see when a new user connects

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
+"use client";
+import { socket } from "@/lib/socketClient";
+import { useEffect } from "react";
+
 // My home page or "/"
 export default function Home() {
   return <h1>Hello, Oliwer!</h1>;
