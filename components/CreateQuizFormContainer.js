8e2db6eb913dc272d34e88cab97765e2e1261eb5
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-06-19 20:42:10 +0200

    fixed the input for everything, now i need to figure how to some one cna click in wich answer is correct in a smart way, then how to add more quesiton, collapse get it all into one questuion object etc

--- components/CreateQuizFormContainer.js
+++ components/CreateQuizFormContainer.js
@@ -1,10 +1,13 @@
 import styles from "@/styles/createQuizFormContainer.module.css";
 import QuizFormTitle from "./QuizFormTitle";
 import DividerLine from "./DividerLine";
+import QuestionForm from "./QuestionForm";
 const CreateQuizFormContainer = () => {
   return (
     <div className={styles.createQuizFormContainerContainer}>
       <QuizFormTitle></QuizFormTitle>
+      <DividerLine></DividerLine>
+      <QuestionForm></QuestionForm>
       <DividerLine></DividerLine>
     </div>
   );
