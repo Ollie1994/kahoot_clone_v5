3ac5a817ab203d7bb37339ff0722d60612061222
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-06-19 16:58:59 +0200

    basic done

--- components/QuizFormTitle.js
+++ components/QuizFormTitle.js
@@ -1,6 +1,23 @@
-"use client"
+"use client";
 import styles from "@/styles/quizFormTitle.module.css";
+import { useState } from "react";
 const QuizFormTitle = () => {
+  const [title, setTitle] = useState("");
+
+  /* const [quiz, setQuiz] = useState({
+    title: "",
+    isLive: false,
+    createdAt: Date.now(),
+    players: [{ username: "", score: 0 }],
+    questions: [
+      {
+        question: "",
+        imageUrl: "",
+        answers: [{ answer: "", isCorrect: Boolean }],
+      },
+    ],
+  }); */
+
   return (
     <div className={styles.quizFormTitleContainer}>
       <h1 className={styles.createQuizFormTitle}>Create Quiz Form</h1>
@@ -15,7 +32,10 @@ const QuizFormTitle = () => {
             className={styles.titleFormInput}
             type="text"
             placeholder="Enter a title for your quiz"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
+          <div>{console.log(`Quiz Title: ${title}`)}</div>
         </div>
       </div>
     </div>
