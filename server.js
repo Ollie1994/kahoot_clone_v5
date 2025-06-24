e73494afe3c1d516e11221f34f0f8ccd2f979926
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-06-24 18:12:44 +0200

    some form of basic room with socket.io

--- server.js
+++ server.js
@@ -12,21 +12,49 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const httpServer = createServer(handle);
   const io = new Server(httpServer);
+  const roomPlayers = {};
   io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
     socket.on("join-room", ({ room, username }) => {
       socket.join(room);
+      // store data
+      socket.data.room = room;
+      socket.data.username = username;
+      // Add player to room memory
+      if (!roomPlayers[room]) {
+        roomPlayers[room] = [];
+      }
+      // Prevent duplicates (in case of reconnect)
+      if (!roomPlayers[room].includes(username)) {
+        roomPlayers[room].push(username);
+      }
+      io.to(room).emit("players_list", roomPlayers[room]);
       console.log(`User ${username} joined room ${room}`);
-      socket.to(room).emit("user_joined", `${username} joined room ${room}`);
     });
-    io.on("disconnect", () => {
+   /*  io.on("disconnect", () => {
       console.log(`User disconnected: ${socket.id}`);
-    });
+    }); */
     // ------------------- test grejer ---------------------------
     socket.on("ready", () => {
       // Send message to everyone
       io.emit("message", "Everyone gets this because client said ready!!!!");
       // --------------------------------------------------------
+    });
+
+    // Handle disconnect
+    socket.on("disconnect", () => {
+      const room = socket.data.room;
+      const username = socket.data.username;
+
+      if (room && username && roomPlayers[room]) {
+        // Remove the user from the room player list
+        roomPlayers[room] = roomPlayers[room].filter((u) => u !== username);
+
+        // Emit the updated player list
+        io.to(room).emit("players_list", roomPlayers[room]);
+
+        console.log(`User ${username} disconnected from room ${room}`);
+      }
     });
   });
 
