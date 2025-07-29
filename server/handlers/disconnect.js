4b861591306f9ccebdd2e267a24ebf0d259eb13e
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2025-07-29 16:39:23 +0200

    added cleanup in DC jsut incase game ends before reachign scoreboard to, thsi way we prevent data getting saved that should be deleted.

--- server/handlers/disconnect.js
+++ server/handlers/disconnect.js
@@ -1,4 +1,9 @@
-import { roomPlayers } from "../states/states.js"
+import { roomPlayers } from "../states/states.js";
+import { roomTimers } from "../states/states.js";
+import { roomScores } from "../states/states.js";
+import { roomState } from "../states/states.js";
+import { quizzes } from "../states/states.js";
+
 export function disconnect(socket, io) {
   const room = socket.data.room;
   const username = socket.data.username;
@@ -11,5 +16,33 @@ export function disconnect(socket, io) {
     io.to(room).emit("players_list", roomPlayers[room]);
 
     console.log(`User ${username} disconnected from room ${room}`);
+
+    // Check if the room is now empty
+    if (roomPlayers[room] && roomPlayers[room].length === 0) {
+
+      console.log(`Room ${room} is now empty`);
+
+      if (roomPlayers[room]) {
+        delete roomPlayers[room];
+        console.log(roomPlayers, " --- removed <", room, "> from list");
+      }
+      if (roomTimers[room]) {
+        clearInterval(roomTimers[room]);
+        delete roomTimers[room];
+        console.log(roomTimers, " --- removed timer <", room, "> from list");
+      }
+      if (roomScores[room]) {
+        delete roomScores[room];
+        console.log(roomScores, " --- removed scores <", room, "> from list");
+      }
+      if (roomState[room]) {
+        delete roomState[room];
+        console.log(roomState, " --- removed state <", room, "> from list");
+      }
+      if (quizzes[room]) {
+        delete quizzes[room];
+        console.log(quizzes, " --- removed quiz <", room, "> from list");
+      }
+    }
   }
 }
